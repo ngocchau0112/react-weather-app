02a0104686d190d3d596d3480d8369f50dc52309
ngocchau0112 <ngocchau0112@gmail.com>
2021-04-21 17:03:01 +0700

    Changed width size of background image

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <div className="container d-flex justify-content-center">
       <div
-        className="w-50 bg-image"
+        className="mx-auto bg-image"
         style={{
+          width: `556px`,
           backgroundImage: `url("https://s3.amazonaws.com/shecodesio-production/uploads/files/000/007/577/original/Night-950px.png?1618979002")`,
         }}
       >
